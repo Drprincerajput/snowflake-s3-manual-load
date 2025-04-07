24fd726ecadfa2972fdcb1c6237e0a21bc10813f
Prince Rajput <drprincerajput@gmail.com>
2025-04-07 17:34:30 +0530

    Delete 04

--- 04_create_external_stage.sql
+++
@@ -1,5 +0,0 @@
--- Create external stage pointing to S3 CSV folder
-CREATE OR REPLACE STAGE MANAGE_DB.external_stages.csv_folder
-    URL = 's3://snowflakewithsnowpipesalesproject/csv/'
-    STORAGE_INTEGRATION = s3_int
-    FILE_FORMAT = MANAGE_DB.file_formats.csv_fileformat;
