e2618b091db1358ff5cb8cf5d4302d1eaebbf251
Prince Rajput <drprincerajput@gmail.com>
2025-04-07 17:34:17 +0530

    Delete 03

--- 03_create_file_format.sql
+++
@@ -1,8 +0,0 @@
--- Create file format object
-CREATE OR REPLACE FILE FORMAT MANAGE_DB.file_formats.csv_fileformat
-    TYPE = CSV
-    FIELD_DELIMITER = ','
-    SKIP_HEADER = 1
-    NULL_IF = ('NULL', 'null')
-    EMPTY_FIELD_AS_NULL = TRUE
-    FIELD_OPTIONALLY_ENCLOSED_BY = '"';
